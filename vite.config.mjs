91618dbd6966493b9286cf0e31d5e7d6da8e6d76
Amna Hashim <amnahashim.contact@gmail.com>
2025-10-16 20:54:26 +0400

    fix: use ESM vite config for GitHub Actions build

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/amna-portfolio/', // 👈 add this line (MUST match your repo name)
+  base: '/amna-portfolio/',
 });
